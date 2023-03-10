c4c541d252ea4ff201a35b07ad841e126ce1f814
nitish-aajtak <nitish.saini@aajtak.com>
2023-03-10 16:03:21 +0530

    creating file to save

--- using express/models/product.js
+++ using express/models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(t){
@@ -6,10 +7,30 @@ module.exports = class Product {
     }
 
     save(){
-        products.push(this);
+        const p = path.join(require.main.filename, 'data' , 'product.json')
+        const product = [];
+        fs.readFile(p,(err,fileContent) =>{
+            if(!err) {
+                product = JSON.parse(fileContent);
+                
+            }
+            product.push(this);
+            fs.writeFile(p,JSON.stringify(product),(err)=>{
+                console.log(err);
+            });
+        })
+        
     }
 
     static fetchAll(){
-        return products;
+        const p = path.join(require.main.filename, 'data' , 'product.json')
+
+        fs.readFile(p , (err,fileContent)=>{
+            if(err){
+                return [];
+            }
+            return JSON.parse(fileContent);
+        });
+        // return products;
     }
 }
